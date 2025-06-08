ec61d813a3e27c92afe6c726c4617d7bab2abb8e
Nishchay <68828647+jasujanish@users.noreply.github.com>
2025-06-08 15:11:03 -0400

    removed glowy effect on landing page because I thought it was tacky

--- src/components/greetings.js
+++ src/components/greetings.js
@@ -19,14 +19,14 @@ const Greeting = () => {
         setIdx((prev) => (prev + 1) % phrases.length);
         setFade('opacity-100'); // Fade in
       }, 500); // Match the transition duration
-    }, 5000); // change every 5 seconds
+    }, 3000); // change every 3 seconds
 
     return () => clearInterval(handle);
   }, []);
 
   return <div className="font-light ">
             <h1 className={`text-[4.5vw] mb-[-2vw] text-gray-300 font-mono transition-opacity duration-500 ${fade}`}> {phrases[idx]} </h1>
-            <h1 className="text-[7.5vw] text-white mb-[1.5vw]"> <span className="hover:bg-gradient-to-r from-purple-500 via-pink-500 to-amber-500 hover:cursor-text hover:bg-clip-text hover:text-transparent">I'm  Nish. </span> </h1>
+            <h1 className="text-[7.5vw] text-white mb-[1.5vw]"> <span className="hover:cursor-text">I'm  Nish. </span> </h1>
         </div>
 };
 
